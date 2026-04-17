f635f14bf8b625216386bdfb386f0921d548a1b9
Luischgz <l.chacon.guzman@gmail.com>
2026-04-17 21:56:05 +0200

    a

--- PFG/src/main/java/com/PFG_LCG/PFG/CampanaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.PFG_LCG.PFG;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CampanaRepository extends JpaRepository<Campana, Integer> {
-}
--- PFG/src/main/java/com/PFG_LCG/PFG/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.PFG_LCG.PFG;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByCorreo(String correo);
-
-}
